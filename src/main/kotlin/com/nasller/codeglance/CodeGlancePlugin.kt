98359997900d66bfd3c8c2053f806ce6cadb4153
Nasller <nasller@qq.com>
2022-03-22 15:36:28 +0800

    init

--- src/main/kotlin/com/nasller/codeglance/CodeGlancePlugin.kt
+++ src/main/kotlin/com/nasller/codeglance/CodeGlancePlugin.kt
@@ -1,12 +1,11 @@
 package com.nasller.codeglance
 
-import com.intellij.openapi.components.NamedComponent
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.fileEditor.FileEditorManagerListener
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
 
-class CodeGlancePlugin : ProjectManagerListener,NamedComponent {
+class CodeGlancePlugin : ProjectManagerListener {
     private val logger = Logger.getInstance(javaClass)
 
     override fun projectOpened(project: Project) {
@@ -14,9 +13,5 @@ class CodeGlancePlugin : ProjectManagerListener,NamedComponent {
             EditorPanelInjector(project)
         )
         logger.debug("CodeGlance initialized")
-    }
-
-    override fun getComponentName(): String {
-        return "CodeGlancePlugin"
     }
 }
