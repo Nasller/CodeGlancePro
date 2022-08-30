03b5fb36aa5955c335f58ac15172b0440ccec774
Nasller <nasller@qq.com>
2022-08-30 17:39:19 +0800

    fix an error in custom fold region

--- src/main/kotlin/com/nasller/codeglance/listener/MyVcsListener.kt
+++ src/main/kotlin/com/nasller/codeglance/listener/MyVcsListener.kt
@@ -1,25 +1,56 @@
 package com.nasller.codeglance.listener
 
+import com.intellij.openapi.Disposable
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.editor.event.DocumentEvent
 import com.intellij.openapi.editor.event.VisibleAreaEvent
 import com.intellij.openapi.editor.event.VisibleAreaListener
 import com.intellij.openapi.editor.ex.PrioritizedDocumentListener
+import com.intellij.openapi.editor.ex.RangeHighlighterEx
+import com.intellij.openapi.editor.impl.event.MarkupModelListener
+import com.intellij.openapi.util.Disposer
 import com.nasller.codeglance.panel.vcs.MyVcsPanel
+import java.awt.event.ComponentAdapter
+import java.awt.event.ComponentEvent
 
-class MyVcsListener(private val myVcsPanel: MyVcsPanel) : PrioritizedDocumentListener, VisibleAreaListener {
+class MyVcsListener(private val myVcsPanel: MyVcsPanel) : ComponentAdapter(), PrioritizedDocumentListener, VisibleAreaListener,
+    MarkupModelListener, Disposable {
     init {
         val editor = myVcsPanel.glancePanel.editor
-        editor.document.addDocumentListener(this,editor.disposable)
-        editor.scrollingModel.addVisibleAreaListener(this,editor.disposable)
+        val disposable = editor.disposable
+        Disposer.register(disposable, this)
+        editor.contentComponent.addComponentListener(this)
+        editor.document.addDocumentListener(this, disposable)
+        editor.scrollingModel.addVisibleAreaListener(this, disposable)
+        editor.filteredDocumentMarkupModel.addMarkupModelListener(disposable, this)
     }
-    /** PrioritizedDocumentListener */
-    override fun documentChanged(event: DocumentEvent) = if(!event.document.isInBulkUpdate) myVcsPanel.repaint() else Unit
+    /** ComponentAdapter */
+    override fun componentResized(componentEvent: ComponentEvent?) = repaint()
 
-    override fun bulkUpdateFinished(document: Document) = myVcsPanel.repaint()
+    /** PrioritizedDocumentListener */
+    override fun documentChanged(event: DocumentEvent) = if(!event.document.isInBulkUpdate) repaint() else Unit
+
+    override fun bulkUpdateFinished(document: Document) = repaint()
 
     override fun getPriority(): Int = 180 //EditorDocumentPriorities
 
     /** VisibleAreaListener */
-    override fun visibleAreaChanged(e: VisibleAreaEvent) = myVcsPanel.repaint()
+    override fun visibleAreaChanged(e: VisibleAreaEvent) = repaint()
+
+    /** MarkupModelListener */
+    override fun afterAdded(highlighter: RangeHighlighterEx) = repaint(highlighter)
+
+    override fun beforeRemoved(highlighter: RangeHighlighterEx) = repaint(highlighter)
+
+    override fun attributesChanged(highlighter: RangeHighlighterEx, renderersChanged: Boolean, fontStyleChanged: Boolean) = repaint(highlighter)
+
+    private fun repaint(highlighter: RangeHighlighterEx? = null) {
+        val editor = myVcsPanel.glancePanel.editor
+        if(myVcsPanel.isVisible && (highlighter == null ||
+                    (highlighter.isThinErrorStripeMark && highlighter.getErrorStripeMarkColor(editor.colorsScheme) != null))) myVcsPanel.repaint()
+    }
+
+    override fun dispose() {
+        myVcsPanel.glancePanel.editor.contentComponent.removeComponentListener(this)
+    }
 }
