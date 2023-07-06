64d888f1879e2cb9e80c1208361ebc42c7917769
Nasller <nasller@qq.com>
2023-07-06 15:13:08 +0800

    change default config

--- src/main/kotlin/com/nasller/codeglance/config/CodeGlanceConfig.kt
+++ src/main/kotlin/com/nasller/codeglance/config/CodeGlanceConfig.kt
@@ -43,7 +43,7 @@ class CodeGlanceConfig : BaseState() {
 	var diffTwoSide by property(true)
 	var diffThreeSide by property(true)
 	var diffThreeSideMiddle by property(false)
-	var editorKinds by listOf(EditorKind.values().toMutableList())
+	var editorKinds by listOf(mutableListOf(EditorKind.MAIN_EDITOR, EditorKind.CONSOLE, EditorKind.PREVIEW, EditorKind.DIFF))
 	var mainWidth by property(110)
 	var diffWidth by property(50)
 	var unTypedWidth by property(50)
