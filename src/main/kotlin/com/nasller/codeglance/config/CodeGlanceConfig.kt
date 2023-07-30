df3a1163040995dc2e80841b76ca8c8b61e2a886
Nasller <nasller@qq.com>
2023-07-30 16:55:48 +0800

    Experimental: Use FastMinimap For Main Editor

--- src/main/kotlin/com/nasller/codeglance/config/CodeGlanceConfig.kt
+++ src/main/kotlin/com/nasller/codeglance/config/CodeGlanceConfig.kt
@@ -9,7 +9,7 @@ import com.nasller.codeglance.config.enums.MouseJumpEnum
 
 class CodeGlanceConfig : BaseState() {
 	var pixelsPerLine by property(4)
-	var maxLinesCount by property(500000)
+	var maxLinesCount by property(100000)
 	var moreThanLineDelay by property(3000)
 	var disabled by property(false)
 	var singleFileVisibleButton by property(true)
