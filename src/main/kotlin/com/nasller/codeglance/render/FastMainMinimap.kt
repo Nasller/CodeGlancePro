b6c74517ed2b6eedfbb94e06dc3e6c897e21f2bb
Nasller <nasller@qq.com>
2023-07-30 18:38:05 +0800

    fix highlight error

--- src/main/kotlin/com/nasller/codeglance/render/FastMainMinimap.kt
+++ src/main/kotlin/com/nasller/codeglance/render/FastMainMinimap.kt
@@ -350,6 +350,7 @@ class FastMainMinimap(glancePanel: GlancePanel, private val isLogFile: Boolean) 
 	override fun dispose() {
 		super.dispose()
 		renderDataList.clear()
+		highlighterChangeList.clear()
 	}
 
 	private data class LineRenderData(val renderData: Array<RenderData>, val startX: Int, val y: Int, val aboveBlockLine: Int, val lineType: LineType = LineType.CODE,
