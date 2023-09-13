a97efb1f5a7e07ea7061960b12b7c6962e855507
Nasller <nasller@qq.com>
2023-09-13 20:53:49 +0800

    replace toggle id

--- src/main/kotlin/com/nasller/codeglance/extensions/GlanceVisibleActionProvider.kt
+++ src/main/kotlin/com/nasller/codeglance/extensions/GlanceVisibleActionProvider.kt
@@ -12,7 +12,7 @@ import com.nasller.codeglance.panel.GlancePanel.Companion.CURRENT_GLANCE
 
 private class GlanceVisibleActionProvider : InspectionWidgetActionProvider {
 	override fun createAction(editor: Editor): AnAction {
-		return object : DefaultActionGroup(ActionManagerEx.getInstanceEx().getAction("CodeGlance.toggle")) {
+		return object : DefaultActionGroup(ActionManagerEx.getInstanceEx().getAction("CodeGlancePro.toggle")) {
 			override fun update(e: AnActionEvent) {
 				e.presentation.isEnabledAndVisible = editor.getUserData(CURRENT_GLANCE)?.run {
 					editor.editorKind == EditorKind.MAIN_EDITOR && !config.hoveringToShowScrollBar && config.singleFileVisibleButton()
