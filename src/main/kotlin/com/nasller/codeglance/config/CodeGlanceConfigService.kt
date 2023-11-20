8816af84380b9a95fcf28f2bcf7cf3b3fee086c1
Nasller <nasller@qq.com>
2023-11-20 10:02:36 +0800

    fix setting export

--- src/main/kotlin/com/nasller/codeglance/config/CodeGlanceConfigService.kt
+++ src/main/kotlin/com/nasller/codeglance/config/CodeGlanceConfigService.kt
@@ -1,13 +1,11 @@
 package com.nasller.codeglance.config
 
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.SimplePersistentStateComponent
 import com.intellij.openapi.components.State
 import com.intellij.openapi.components.Storage
 import com.nasller.codeglance.util.Util
 
-@Service(Service.Level.APP)
 @State(name = Util.PLUGIN_NAME, storages = [Storage("CodeGlancePro.xml")])
 class CodeGlanceConfigService : SimplePersistentStateComponent<CodeGlanceConfig>(CodeGlanceConfig()) {
 	companion object {
