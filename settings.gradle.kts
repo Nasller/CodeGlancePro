c8ee3364162071c5e2ea85a97356b060828c6a23
Nasller <nasller@qq.com>
2024-11-19 10:34:15 +0800

    repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,13 @@
+@file:Suppress("UnstableApiUsage")
+
 rootProject.name = "CodeGlancePro"
 include(":core")
 include(":rider")
+
+dependencyResolutionManagement {
+	repositories {
+		maven("https://mirrors.tencent.com/nexus/repository/maven-public/")
+		mavenLocal()
+		mavenCentral()
+	}
+}
