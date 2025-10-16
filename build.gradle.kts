4252d0247fabdb7b98ed3d4a89bc0d22fd713bfa
Nasller <nasller@qq.com>
2025-10-16 09:58:01 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
-import org.jetbrains.intellij.platform.gradle.TestFrameworkType
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
@@ -34,7 +33,7 @@ dependencies {
         bundledModules(properties("platformBundledModules").map { it.split(',') })
 
 		zipSigner()
-        testFramework(TestFrameworkType.Platform)
+//        testFramework(TestFrameworkType.Platform)
 	}
 }
 
